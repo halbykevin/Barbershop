971a1e7560cab16f2db0839096daea4f44d990d3
JimmyNassar <153899825+JimmyNassar@users.noreply.github.com>
2024-05-17 15:13:31 +0300

    added the list of transactions

--- box.php
+++ box.php
@@ -11,6 +11,28 @@
       <div class="left-button">
         <a href="index.php"><button class="back-button">Back</button></a>
       </div>
+      <!-- fix the placement and styling of the listed transactions -->
+      <div class = "transaction-list">
+      <ul>
+      <?php
+      require "connections.php";
+       $sql = mysqli_query($conn, "SELECT * FROM `transaction`");
+       while ($row = mysqli_fetch_array($sql)) {
+           $amount =$row['amount'];
+           $date =$row['date'];
+           $details =$row['details'];
+           $type = $row['type'];
+           if($type == 'checkin'){
+            echo "<li> Recieved $amount $ on $date, from $details</li>";
+           }
+           else{
+            echo "<li> Payed $amount $ on $date: $details</li>";
+           }
+           
+       }
+      ?>
+      </ul>
+      </div>
     </div>
   </body>
 </html>
